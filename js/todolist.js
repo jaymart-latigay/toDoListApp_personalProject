121f2fd07321f49d96c62e3f02ebf2d10cf099bf
jaymart-latigay <jglatigay@up.edu.ph>
2024-02-09 22:15:18 +0800

    chore: finished ToDoList js class

---
+++ js/todolist.js
@@ -0,0 +1,29 @@
+export default class TodoList {
+  constructor() {
+    this._list = []; // Array to store the list of tasks
+  }
+
+  getList() {
+    return this._list; // Return the list of tasks
+  }
+
+  clearList() {
+    this._list = []; /* Clear the list of tasks */
+  }
+
+  addItemToList(itemObj) {
+    this._list.push(itemObj); // Add the task to the list
+  }
+
+  removeItemFromList(id) {
+    const list = this._list;
+
+    for (let i = 0; i < list.length; i++) {
+      if (list[i]._id == id) {
+        /* == : since id is a string when extracted from DOM */
+        list.splice(i, 1); //Remove the task from the list
+        break;
+      }
+    }
+  }
+}
